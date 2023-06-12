0edbeda17bf891cf0675d7d3fc36680ca5635348
tone_herndon <tonecloudsys@gmail.com>
2023-06-12 18:14:47 -0400

    Update

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,9 +1,36 @@
-resource "aws_dynamodb_table" "resume-counter-table" {
+resource "aws_dynamodb_table_item" "counter-table" {
    
    lifecycle {
     ignore_changes = [
         item,
     ]
    }
-    table_name = "ResumeCounter"
+
+   table_name = aws_dynamodb_table.counter-table.name
+   hash_key = aws_dynamodb_table.counter-table.hash_key
+
+   item = <<ITEM
+
+{
+    "primaryKey": {"S": "VisitorCounter"},
+    "counterValue": {"N": "0"}
 }
+   
+   ITEM
+}
+
+
+
+resource "aws_dynamodb_table" "counter-table" {
+    name = "counter-table"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "primarykey"
+   
+
+    attribute {
+        name = "primarykey"
+        type = "S"
+    }
+ 
+    }
+
