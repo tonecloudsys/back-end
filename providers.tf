5003373e71c8f07d613333843cfec6864c6af372
tone_herndon <tonecloudsys@gmail.com>
2023-07-12 14:17:45 -0400

    Update

--- providers.tf
+++ providers.tf
@@ -24,5 +24,5 @@ locals {
     account_id  = "44886408-4031-7010-7a27-0d8c4952c206"
     region      = "us-east-1"
     root_domain_name = "resume.toneherndon.com"
-    cv_domain_name = "cv.${local.root_domain_nmae}"
+    cv_domain_name = "cv.${local.root_domain_name}"
 }
