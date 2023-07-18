7f90def60ede3ff70f1965c664b7cf86a85063c6
tone_herndon <tonecloudsys@gmail.com>
2023-07-18 16:39:38 -0400

    Update

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,3 +1,16 @@
+resource "aws_dynamodb_table" "counter-table" {
+    name = "counter-table"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "CounterID"
+   read_capacity = 0
+   write_capacity = 0
+
+    attribute {
+        name = "CounterID"
+        type = "S"
+    }
+}
+
 resource "aws_dynamodb_table_item" "counter-table" {
    
    lifecycle {
@@ -10,27 +23,11 @@ resource "aws_dynamodb_table_item" "counter-table" {
    hash_key = aws_dynamodb_table.counter-table.hash_key
 
    item = <<ITEM
-
 {
-    "primaryKey": {"S": "VisitorCounter"},
-    "counterValue": {"N": "0"}
+    "CounterID" : {"S": "visits"},
+    "count" : {"N": "0"}
 }
-   
-   ITEM
+ITEM
 }
 
-
-
-resource "aws_dynamodb_table" "counter-table" {
-    name = "counter-table"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "CounterID"
-   
-
-    attribute {
-        name = "CounterID"
-        type = "S"
-    }
- 
-    }
 
